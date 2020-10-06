ddc5a3176a18e39f73a4907e7ee3014c9de47d12
Todd G <todd@dapix.io>
2020-10-06 16:19:31 -0600

    make mac package work on 10.14

--- mac-package.sh
+++ mac-package.sh
@@ -2,6 +2,9 @@
 
 # builds a macos package (.app) and places it inside a compressed .dmg
 
+export CGO_CFLAGS="-mmacosx-version-min=10.14"
+export CGO_LDFLAGS="-mmacosx-version-min=10.14"
+
 mkdir -p "package/Cryptonym"
 mkdir -p package/old
 mv package/*.dmg package/old/
